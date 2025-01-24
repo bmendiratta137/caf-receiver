6afa517e1a1f4ec4f411e19b9151d74141a86e78
bmendiratta <bmendiratta@newrelic.com>
2025-01-24 18:21:32 +0530

    eigthth

--- src/tracker.js
+++ src/tracker.js
@@ -11,6 +11,7 @@ export default class CAFTracker extends nrvideo.VideoTracker {
     super()
     this.reset()
     this.registerListeners()
+    console.log("CAFTracker initialized");
   }
 
   registerListeners() {
